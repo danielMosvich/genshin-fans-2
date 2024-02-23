fe4834b23462ae69c15a19bacc76c1d342af3ac3
danielmosvich <mrdaniraymay@gmail.com>
2024-02-23 12:03:36 -0500

    test dark mode

--- src/services/getCharacters.ts
+++ src/services/getCharacters.ts
@@ -3,11 +3,11 @@ import { type CharactersProps } from "../types/characters";
 export async function getCharacters() {
   const res = await fetch("https://genshin.jmp.blue/characters/all");
   const dataAllCharacters = (await res.json()) as CharactersProps[];
-  dataAllCharacters.sort((a,b) => b.rarity - a.rarity)
-  return dataAllCharacters
+  dataAllCharacters.sort((a, b) => b.rarity - a.rarity);
+  return dataAllCharacters;
 }
-export async function getCharacterById(id:string){
-    const res =await fetch(`https://genshin.jmp.blue/characters/${id}`)
-    const dataCharacterById = await res.json() as CharactersProps
-    return dataCharacterById
+export async function getCharacterById(id: string) {
+  const res = await fetch(`https://genshin.jmp.blue/characters/${id}`);
+  const dataCharacterById = (await res.json()) as CharactersProps;
+  return dataCharacterById;
 }
